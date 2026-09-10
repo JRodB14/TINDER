553c32a7c1f499de8b51f3a296627cfc9256f7f0
JRodB14 <julieth.rodriguez@cun.edu.co>
2026-09-09 19:07:57 -0500

    Add files via upload

---
+++ BaseTinder.py
@@ -0,0 +1,57 @@
+def GuardarPersonas():
+
+    nombre= input("Como te llamas?")
+    edad= input("Que edad tienes?")
+    ciudad= input("De donde eres?")
+    genero= input("Cual es tu genero?")
+    gbusca= input("Que genero busca?")
+    edadmin =input("Cual es la edad minima que buscas para tu futura cita?")
+    if edadmin < 18:
+        print("Debe ser mayor de edad")
+        edadmin =input("Por favor coloque una edad permitida")
+    
+    edadmax =input("Cual es la edad maxima que buscas para tu futura cita?")
+    if 120 > edadmax:
+        print("Estas buscando el ataud o que?")
+        edadmax =input("Por favor coloque una edad permitida")
+
+    distancia=input("Cuantos kilometros aceptas que este de distancia tu futura pareja?")
+
+    individuo={"nombre":nombre,"edad":edad,"ciudad":ciudad,"genero":genero,"gbusca":gbusca,"edadmin":edadmin,"edadmax":edadmax,"distancia":distancia}
+    return GuardarPersonas
+
+def RegistrarPersonas():
+    N=input("Cuantas personas vas a registrar?")
+    for i in range (0,N):
+       print("#i",i)
+       print(usuario)
+       usuario[i]= GuardarPersonas()
+       
+
+
+
+
+
+def main ():
+    print ("----------------BIENVENIDO A TINDER CUN--------------")
+    print ("La mejor plataforma para encontrar el amor")
+    print ("")
+    print ("--MENU--")
+    print ("Por favor selecciona el numero de lo que quieres hacer")
+    print ("")
+    print ("1. Para registrar un nuevo usuario")
+    print ("2. Mostrar personas registradas")
+    print ("3. Buscar posibles coincidencias")
+    print ("4. Mostrar el porcentaje de compatibilidad de cada coincidencia")
+    print ("5. Mostrar los intereses que tienen en común")
+    print ("6. Identificar cuál es la persona más compatible")
+    print ("7. Mostrar las personas que no cumplen los requisitos mínimos de compatibilidad")
+    print ("8. Consultar las coincidencias de cualquier persona registrada")
+    menu=input()
+    match menu:
+        
+        case "1":
+         RegistrarPersonas()
+
+
+main()
